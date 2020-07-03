d7296a1854674589f156543a760f7df508eef956
oguzhankirlar <oguzhan_kirlar@hotmail.com>
2020-07-03 21:43:27 +0300

    Update traincorpus.py

--- traincorpus.py
+++ traincorpus.py
@@ -4,7 +4,7 @@ from gensim.models import Word2Vec
 from gensim.models.word2vec import LineSentence
 
 if __name__ == '__main__':
-    inputFile = "/home/oguzhan/PycharmProjects/OguzModel/wiki.tr.txt"
+    inputFile = sys.argv[1] # preprocess.py dosyasından elde ettiğimiz wiki.tr.txt dosyasının linkinin input olarak yazabilirsiniz ya da consoldan input olarak verebilirsiniz
     outputFile = "trmodel"
     model = Word2Vec(LineSentence(inputFile), size=400, window=5, min_count=5, workers=multiprocessing.cpu_count())
     model.wv.save_word2vec_format(outputFile, binary=True)
